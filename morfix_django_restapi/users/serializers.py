b62690d2f0861520f9602228e1e27995a8a10429
Vladislav1234512345 <tolerantniy2104@gmail.com>
2024-10-04 21:37:43 +0400

    Убрал из сериализатора поле password2. Валидация пароля1 и пароля2 будет на фронте

--- morfix_django_restapi/users/serializers.py
+++ morfix_django_restapi/users/serializers.py
@@ -14,21 +14,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[validate_password],
     )
-    password2 = serializers.CharField(
-        write_only=True,
-        required=True,
-    )
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({'password': 'Passwords do not match.'})
-        return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
