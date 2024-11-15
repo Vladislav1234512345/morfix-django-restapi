9ba73f4107d2de9c9d61ac35d84619926711ca7e
Vladislav1234512345 <tolerantniy2104@gmail.com>
2024-11-16 02:57:49 +0400

    fix: requirements.txt, settings.py; feat: .env, .env.celery, .env.db

--- morfix_django_restapi/morfix_django_restapi/settings.py
+++ morfix_django_restapi/morfix_django_restapi/settings.py
@@ -9,44 +9,46 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+import os, logging, redis
 from pathlib import Path
 
 from datetime import timedelta
 
-import logging
-
-import redis
 
 logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_1*t3(z^!fpx9^u3w#s)um29ryz26r(cad)885)wmd71-zpt+l'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-_1*t3(z^!fpx9^u3w#s)um29ryz26r(cad)885)wmd71-zpt+l')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 # Разрешенные хосты
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['*']
+
+hosts = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
+
+ALLOWED_HOSTS = hosts
+
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'morfix_django_restapi',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        # 'HOST': 'localhost',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.contrib.gis.db.backends.postgis'),
+        'NAME': os.environ.get('DB_NAME', 'morfix_django_restapi'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
@@ -134,12 +136,12 @@ MIDDLEWARE = [
 # CORS_ALLOW_ALL_ORIGINS = True
 
 # Разрешеннные источники
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",  # Фронтенд адрес для доверенных запросов с CSRF-токеном
+#     "http://127.0.0.1:5173",
+# ]
+
+CORS_ALLOWED_ORIGINS = ["http://" + host + ":5137" for host in hosts]
 
 CORS_ALLOW_CREDENTIALS = True  # Разрешить передачу данных в куках
 
@@ -164,10 +166,12 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",  # Фронтенд адрес для доверенных запросов с CSRF-токеном
-    "http://127.0.0.1:5173",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:5173",  # Фронтенд адрес для доверенных запросов с CSRF-токеном
+#     "http://127.0.0.1:5173",
+# ]
+
+CSRF_TRUSTED_ORIGINS = ["http://" + host + ":5137" for host in hosts]
 
 ROOT_URLCONF = 'morfix_django_restapi.urls'
 
