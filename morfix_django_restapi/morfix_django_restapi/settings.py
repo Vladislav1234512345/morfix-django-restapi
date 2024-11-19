eeecd4bd24a97990c8ea8164a51346db098779b5
Vladislav1234512345 <tolerantniy2104@gmail.com>
2024-11-20 02:26:22 +0400

    Версия 1.4.5 docker образа vladislav1234512345/morfix-django-restapi включает в себя изменения переменнй cors_allowed_hosts в morfix_django_restapi/settings.py. Данная переменная дает разрешение на получение заголовков и кук, и раньше все источинки и хосты могли отправлять и поулчать куки и заголовки. Но теперь я ограничил оптравку и получение данных по cors только для домена dipluv.ru и его субдоменов, к примеру, www.dipluv.ru, api.dipluv.ru.

--- morfix_django_restapi/morfix_django_restapi/settings.py
+++ morfix_django_restapi/morfix_django_restapi/settings.py
@@ -128,12 +128,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Разрешить все источники
-CORS_ALLOW_ALL_ORIGINS = True
+# # Разрешить все источники
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = ["http://" + host + ":5137" for host in hosts]
+CORS_ALLOWED_ORIGINS = str(
+    os.environ.get(
+        "DJANGO_CORS_ALLOWED_ORIGINS",
+        "http://localhost:5137 http://localhost:8000 http://127.0.0.1:5137 http://127.0.0.1:8000"
+    )
+).split(" ")
 
-CORS_ALLOW_CREDENTIALS = True  # Разрешить передачу данных в куках
+# Разрешить передачу данных в куках
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
     'GET',
@@ -165,8 +171,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://188.120.231.10",
     "http://188.120.231.10:8000",
 ]
-
-# CSRF_TRUSTED_ORIGINS = ["http://" + host + ":5137" for host in hosts]
 
 ROOT_URLCONF = 'morfix_django_restapi.urls'
 
