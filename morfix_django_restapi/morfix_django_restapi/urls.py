f02f406e83b90ad0debecaf3b3dfc8e281c7abcb
Vladislav1234512345 <tolerantniy2104@gmail.com>
2024-11-24 00:22:33 +0400

    В версии 1.5.0 docker образа vladislav1234512345/morfix-django-restapi я добавил swagger и redoc. Их можно найти по эндпоинтам .../swagger/, .../redoc/. (серверная разработка)

--- morfix_django_restapi/morfix_django_restapi/urls.py
+++ morfix_django_restapi/morfix_django_restapi/urls.py
@@ -19,6 +19,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # Изменяем заголовки админ-панели django
 
@@ -29,13 +32,32 @@ admin.site.site_title = "Админ-панель DipLove"
 # Заголовок на главной странице панели
 admin.site.index_title = "Добро пожаловать в панель управления DipLove"
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/user/', include('users.urls')),
     path('api/profile/', include('profiles.urls')),
     path('api/chat/', include('chats.urls')),
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('admin/', permanent=True)),
 ]
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
